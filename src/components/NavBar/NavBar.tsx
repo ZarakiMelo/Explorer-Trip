3f61307c185b0f6ad96ded4116830a6f7b585fdd
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-02 22:30:36 +0200

    Files.json modification to fix fortawesomeIcon error => can't find module

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './NavBar.module.css';  
+import { faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,6 +13,9 @@ const NavBar: React.FC = () => {
 
   return (
     <nav className={styles.navbar}>
+      <div className={styles['menu-icon']} onClick={toggleMenu}>
+        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+      </div>
       <ul className={ styles['nav-menu']}>
         <li className={styles['nav-item']}>
           <Link to="/myTrip" className={styles['nav-links']} onClick={toggleMenu}>Lancer un trip</Link>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,6 @@
 // pages/HomePage.tsx
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-import Quotes from '../components/Quotes/Quotes';
-import Section from '../components/Section/Section';
-import Steps from '../components/Steps/Steps';
-import NewTrip from '../components/NewTrip/NewTrip';
-import Footer from '../components/Footer/Footer';
 
 const HomePage: React.FC = () => {
   return (

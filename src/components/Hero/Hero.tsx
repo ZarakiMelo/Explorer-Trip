f29d63956954f303d38972f4d75e4a264161a7ba
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-03 01:11:39 +0200

    HeroComponent: Navbar & responsive

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,6 +6,9 @@ import styles from './Hero.module.css';  // Import du fichier CSS
 const Hero: React.FC = () => {
   return (
     <div className={styles.container_hero}>
+      <div className={styles.logo_container}> 
+        <img className={styles.logo} src='/appLogo.png' />
+      </div>
       <NavBar/>
     </div>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './NavBar.module.css';  
-import { faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 const NavBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,9 +12,6 @@ const NavBar: React.FC = () => {
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles['menu-icon']} onClick={toggleMenu}>
-        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
-      </div>
       <ul className={ styles['nav-menu']}>
         <li className={styles['nav-item']}>
           <Link to="/myTrip" className={styles['nav-links']} onClick={toggleMenu}>Lancer un trip</Link>

7a3cec026dfc9b0b60f8af94d5520e5f63963898
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-02 16:48:19 +0200

    NavBarComponent: navigationBar with links and styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,24 @@
-// components/Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './NavBar.module.css';  
 
 const NavBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/myTrip">Lancer un trip</Link></li>
-        <li><Link to="/allTrips">Mes voyages</Link></li>
+    <nav className={styles.navbar}>
+      <ul className={ styles['nav-menu']}>
+        <li className={styles['nav-item']}>
+          <Link to="/myTrip" className={styles['nav-links']} onClick={toggleMenu}>Lancer un trip</Link>
+        </li>
+        <li className={styles['nav-item']}>
+          <Link to="/allTrips" className={styles['nav-links']} onClick={toggleMenu}>Mes voyages</Link>
+        </li>
       </ul>
     </nav>
   );

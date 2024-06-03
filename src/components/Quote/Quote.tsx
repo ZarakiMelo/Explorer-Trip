6e2659798a596bebe1f5769b5dbe91f992463adf
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-03 20:37:46 +0200

    QuotesComponent : refacto and QuoteComponent creation

---
+++ src/components/Quote/Quote.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../Quote/Quote.module.css';
+
+interface QuoteProps {
+  text: string;
+  author: string;
+  position: 'left' | 'right';
+}
+
+const Quote: React.FC<QuoteProps> = ({ text, author, position }) => {
+  const containerClass =
+    position === 'left'
+      ? styles.quote_left_container
+      : styles.quote_right_container;
+  return (
+    <div className={containerClass}>
+      <p className={styles.quote_text}>
+        {text} ({author})
+      </p>
+    </div>
+  );
+};
+
+export default Quote;
--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import styles from './Quotes.module.css'
+import styles from './Quotes.module.css';
+import Quote from '../Quote/Quote';
+
+interface QuoteData {
+  text: string;
+  author: string;
+  position: 'left' | 'right';
+}
+
+const quotes: QuoteData[] = [
+  {
+    text: '« Un voyage de mille lieues commence toujours par un premier pas. »',
+    author: 'Lao-Tseu',
+    position: 'left',
+  },
+  {
+    text: '« Le bonheur n’est pas une destination à atteindre, mais une façon de voyager. »',
+    author: 'Margaret Lee Runbeck',
+    position: 'right',
+  },
+  {
+    text: '« Voyager, c’est donner un sens à sa vie, voyager, c’est donner de la vie à ses sens. »',
+    author: 'Alexandre Poussin',
+    position: 'left',
+  },
+];
 
 const Quotes: React.FC = () => {
   return (
     <div className={styles.quotes_container}>
-      <div className={styles.quote_left_container}>
-          <text>« Un voyage de mille lieues commence toujours par un premier pas. » (Lao-Tseu)</text>
-      </div>
-      <div className={styles.quote_right_container}><text>« Le bonheur n’est pas une destination à atteindre, mais une façon de voyager. » (Margaret Lee Runbeck)</text></div>
-      <div className={styles.quote_left_container}><text> « Voyager, c’est donner un sens à sa vie, voyager, c’est donner de la vie à ses sens. » (Alexandre Poussin)</text></div>
+      {quotes.map((quote, index) => (
+        <Quote
+          key={index}
+          text={quote.text}
+          author={quote.author}
+          position={quote.position}
+        />
+      ))}
     </div>
   );
 };

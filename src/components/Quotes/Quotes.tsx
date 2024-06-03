158ce7e97e76e82f7865c48ab15e0d98e55e0301
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-03 19:31:37 +0200

    QuotesComponent : responsive structure

--- src/components/Quotes/Quotes.tsx
+++ src/components/Quotes/Quotes.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Quotes.module.css'
 
 const Quotes: React.FC = () => {
   return (
-    <div>
-      Quotes
+    <div className={styles.quotes_container}>
+      <div className={styles.quote_left_container}>
+          <text>« Un voyage de mille lieues commence toujours par un premier pas. » (Lao-Tseu)</text>
+      </div>
+      <div className={styles.quote_right_container}><text>« Le bonheur n’est pas une destination à atteindre, mais une façon de voyager. » (Margaret Lee Runbeck)</text></div>
+      <div className={styles.quote_left_container}><text> « Voyager, c’est donner un sens à sa vie, voyager, c’est donner de la vie à ses sens. » (Alexandre Poussin)</text></div>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 // pages/HomePage.tsx
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Quotes from '../components/Quotes/Quotes';
+
 
 const HomePage: React.FC = () => {
   return (
       <>
       <Hero />
+      <Quotes/>
       </>
       
   

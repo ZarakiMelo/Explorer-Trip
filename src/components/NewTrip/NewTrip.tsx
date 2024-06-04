b354e7f834763e0dc5fb05cc57ebdfe78e42c57a
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-04 20:02:06 +0200

    NewTrip component: add react-calendar component & styled

--- src/components/NewTrip/NewTrip.tsx
+++ src/components/NewTrip/NewTrip.tsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
 import styles from './NewTrip.module.css';
 import MainNavBar from '../MainNavBar/MainNavBar';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../../styles/CustomCalendar.css';
 
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const NewTrip: React.FC = () => {
-  const [trip, setTrip] = useState("");
-  const [startDay,setStartDay] = useState("");
+  const [trip, setTrip] = useState<string>("");
+  const [date, setDate] = useState<Value>(new Date());
 
 
   const handleSubmit = () => {
     if(!trip)console.log("Saisir un nom de voyage");
     else
-    if(!startDay) console.log("Saisir une date de début");
-    else console.log(`${trip} débutera le ${startDay} !`);
+    if(!date) console.log("Saisir une date de début");
+    else console.log(`${trip} débutera le ${date} !`);
     }
 
   return (
     <div className={styles.container}>
       <MainNavBar />
-      <div className={styles.inputs_container}>
         <div className={styles.form_container} >
             <p className={styles.text}>L'aventure s'appelera</p>
             <input className={styles.input} type="text" value={trip} onChange= {(e)=>{setTrip(e.target.value)}} />
             <p className={styles.text}>et elle débutera le :</p>
-          
+            <Calendar onChange={setDate} value={date}/>
             <button className={styles.submit_button} type="submit" onClick={handleSubmit}>C'est parti !</button>
         </div>
-      </div>
     </div>
   );
 };

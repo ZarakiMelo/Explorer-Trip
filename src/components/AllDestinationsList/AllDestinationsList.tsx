bed5747f2fa6e7cc70a5558f5c9c691bf06fae5d
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-10 09:16:47 +0200

    Destinations list : sorted display

--- src/components/AllDestinationsList/AllDestinationsList.tsx
+++ src/components/AllDestinationsList/AllDestinationsList.tsx
@@ -10,15 +10,34 @@ const AllDestinationsList: React.FC<AllDestinationsListProps> = ({allDestination
   const openCloseModal = () => {
     setModal(!modal)
   }
-
+/*
 const displayLocations =
 allDestinations.map((location)=>{
   if(location.dates && !(location.dates instanceof Date)){
     //console.log(typeof(location.dates[0]))
   }
     return    <DestinationCard key={location.id} destination={location} modal={modal} handleDeleteDestination={handleDeleteDestination} openCloseModal={openCloseModal} />;
-});
-
+});*/
+const displayLocations = allDestinations
+  .filter((location) => {
+    return Array.isArray(location.dates) && location.dates[0] instanceof Date;
+  })
+  .sort((a, b) => {
+    const dateA = new Date((a.dates as [Date, Date])[0]).getTime();
+    const dateB = new Date((b.dates as [Date, Date])[0]).getTime();
+    return dateA - dateB; // Trier par date de début (ascendant)
+  })
+  .map((location) => {
+    return (
+      <DestinationCard
+        key={location.id}
+        destination={location}
+        modal={modal}
+        handleDeleteDestination={handleDeleteDestination}
+        openCloseModal={openCloseModal}
+      />
+    );
+  });
 
   return (
     <div className={styles.list_container}>
